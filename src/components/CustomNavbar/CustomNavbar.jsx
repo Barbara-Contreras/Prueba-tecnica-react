3e30c0c2f1f111f84a034f926b2a03a0edc66909
Bárbara Contreras <bcontreraslagos@gmail.com>
2024-06-18 23:41:25 -0400

    Iniciando menú hamburguesa

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -2,16 +2,27 @@ import { Logo } from './Logo/Logo.jsx'
 import { Section } from './Section/Section.jsx'
 import './CustomNavbar.css'
 import PropTypes from 'prop-types';
+import { Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export function CustomNavbar({ sections }) {
     return (
-        <div className='custom-navbar-style'>
+        <div className="custom-navbar-style">
             <Logo />
-            <div className="custom-nav-sections">
-                {sections.map((section, index) => (
-                    <Section key={index} name={section.name} />
-                ))}
+            <div className='custom-navbar-container'>
+                <Navbar collapseOnSelect expand="lg">
+                    <Navbar.Brand href="#home">
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            {sections.map((section, index) => (
+                                <Section key={index} name={section.name} />
+                            ))}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </div>
         </div>
     );
